23c5a8473766f2db438315d47ef5b935b2143d06
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2018-12-04 21:24:22 +0000

    2018-day01 part 2

--- src/aoc2018/day01/day01.kt
+++ src/aoc2018/day01/day01.kt
@@ -3,7 +3,35 @@ package aoc2018.day01
 fun main(args: Array<String>) {
     val input = preprocess(rawInput)
 
-    val finalFrequency = input.fold(0, Int::plus)
+    println("Input size: ${input.size}")
 
-    println("Result: $finalFrequency")
+//    val result = finalFrequency(input)
+    val result = firstRepeatedFrequency(input)
+
+    println("Result: $result")
+}
+
+// part 1
+fun finalFrequency(input: List<Int>) = input.fold(0, Int::plus)
+
+// part 2
+fun firstRepeatedFrequency(input: List<Int>) : Int {
+    // this will be filled while we compute new frequencies; it's important it's a set
+    val observedFreqs = mutableSetOf(0)
+
+    // this repeats values from 'input' infinitely
+    val freqChanges: Sequence<Int> = generateSequence { input }.flatten()
+
+    freqChanges.fold(0, {currentFreq, freqChange ->
+        val newFreq = currentFreq + freqChange
+
+        // we use the fact that a set returns false when trying to add a duplicate
+        val freqWasSeenPreviously = !observedFreqs.add(newFreq)
+
+        if (freqWasSeenPreviously) return@firstRepeatedFrequency newFreq
+
+        newFreq
+    })
+
+    throw Exception("This line is needed to compile but never gets executed")
 }
