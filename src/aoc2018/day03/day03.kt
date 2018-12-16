147fd78c4fe21b472dd1ec793cf5fb161b77c214
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2018-12-16 21:51:23 +0000

    2018-day03 preprocess input

--- src/aoc2018/day03/day03.kt
+++ src/aoc2018/day03/day03.kt
@@ -14,12 +14,14 @@ fun main(args: Array<String>){
 }
 
 
-fun countTotalOverlap(claims: List<ElfClaim>): Int{
+fun countTotalOverlap(claims: List<ElfClaim>): Int {
+    // compute how much surface must be analyzed, how many rows to analyze
     val maxStrip: Int = claims
             .maxBy { it.top + it.height - 1 }
             ?.let { it.top + it.height }
             ?: throw Exception("wat? no claims?")
 
+    // for each row countOverlapInStrip
     return (1..maxStrip)
             .fold(0) { currentTotal, strip ->
                 currentTotal + countOverlapInStrip(strip, claims)
@@ -28,10 +30,14 @@ fun countTotalOverlap(claims: List<ElfClaim>): Int{
 
 
 fun countOverlapInStrip(strip: Int, allClaims: List<ElfClaim>): Int {
+    // filter out claims not intersecting with the strip (optimization)
     val claims: List<ElfClaim> = claimsApplicableToStrip(strip, allClaims)
 
+    // create a hashmap which counts how many claims there are for each inch in the strip
+    // (i.e. for each cell in the row)
     val claimedInchesInStrip = claims
             .fold(hashMapOf<Int, Int>()) { claimedInches, claim ->
+                // add intersection of claim and strip to the result
                 (claim.left until (claim.left + claim.width))
                         .forEach { claimedInches.merge(it, 1, Int::plus) }
 
@@ -39,6 +45,7 @@ fun countOverlapInStrip(strip: Int, allClaims: List<ElfClaim>): Int {
             }
 
     return claimedInchesInStrip
+            // count only inches claimed by more than one elf
             .filter { it.value > 1 }
             .size
 }
