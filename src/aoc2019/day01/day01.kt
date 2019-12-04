cc9a6553c192f9867da8268a7323b72a14c8156c
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2019-12-04 21:21:51 +0000

    2019-day01 part 1

--- src/aoc2019/day01/day01.kt
+++ src/aoc2019/day01/day01.kt
@@ -1,5 +1,11 @@
 package aoc2019.day01
 
 fun main(args: Array<String>) {
-    println("2019")
+    val input = preprocess(rawInput)
+
+    val part1: Int = input
+            .map { x -> (x / 3) - 2 }
+            .sum()
+    println("part1: $part1")
 }
+
