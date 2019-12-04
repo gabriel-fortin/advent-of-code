46d0cbdad5145401adf1a6cc99ac904f591b3ac3
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2019-12-04 21:36:01 +0000

    2019-day01 part 2

--- src/aoc2019/day01/day01.kt
+++ src/aoc2019/day01/day01.kt
@@ -1,9 +1,13 @@
 package aoc2019.day01
 
+import kotlin.math.max
+
 fun main(args: Array<String>) {
     val input = preprocess(rawInput)
 
     println("part1: ${part1(input)}")
+
+    println("part2: ${part2(input)}")
 }
 
 fun part1(masses: List<Int>): Int {
@@ -12,7 +16,27 @@ fun part1(masses: List<Int>): Int {
             .sum()
 }
 
+fun part2(masses: List<Int>): Int {
+    return masses
+            .map(::calculateReappliedFuelRequirementForMass)
+            .sum()
+}
+
 fun calculateFuelRequirementForMass(mass: Int): Int {
     return (mass / 3) - 2
+}
+
+fun calculateReappliedFuelRequirementForMass(mass: Int): Int {
+    var totalFuelRequirement = 0
+    var partialFuelReq: Int
+    var currentMass = mass
+
+    do {
+        partialFuelReq = max(0, calculateFuelRequirementForMass(currentMass))
+        totalFuelRequirement += partialFuelReq
+        currentMass = partialFuelReq
+    } while (partialFuelReq > 0)
+
+    return totalFuelRequirement
 }
 
