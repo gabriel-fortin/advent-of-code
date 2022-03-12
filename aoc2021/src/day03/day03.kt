5c80dfc1a34f91954ae407b36a408d6ce6714e33
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2022-03-12 11:54:56 +0000

    Add 2021 - day 3 - part 2

--- aoc2021/src/day03/day03.kt
+++ aoc2021/src/day03/day03.kt
@@ -1,19 +1,20 @@
 package day03
 
 // defined after having visually inspected the input
-const val NUMBER_LENGTH = 12
+const val DIGIT_COUNT = 12
 
 fun main() {
     val inputNumbers = readInputNumbers()
 
     println("part 1:  ${part1(inputNumbers)}")
+    println("part 2:  ${part2(inputNumbers)}")
 }
 
-private fun part1(inputNumbers: List<Int>): Int {
-    val mostCommonBitCounter = MutableList<Double>(NUMBER_LENGTH) { .0 }
+fun part1(inputNumbers: List<Int>): Int {
+    val mostCommonBitCounter = MutableList<Double>(DIGIT_COUNT) { .0 }
     for (inputNumber in inputNumbers) {
         var number = inputNumber
-        for (bitPos in 0 until NUMBER_LENGTH) {
+        for (bitPos in 0 until DIGIT_COUNT) {
             // get last bit
             val bit = number and 1
             // if we represent bits as +/-0.5 then
@@ -31,8 +32,43 @@ private fun part1(inputNumbers: List<Int>): Int {
             2 * gammaRate + mostCommonBit
         }
 
-    val bitMask = ((1 shl NUMBER_LENGTH) - 1)
+    val bitMask = ((1 shl DIGIT_COUNT) - 1)
     val epsilonRate = gammaRate xor bitMask
 
     return gammaRate * epsilonRate
 }
+
+fun part2(inputNumbers: List<Int>): Int {
+    val oxygenGeneratorRating = reduceIntoRating(inputNumbers, bitCriteriaIsMostCommonBit = true)
+    val scrubberRating = reduceIntoRating(inputNumbers, bitCriteriaIsMostCommonBit = false)
+    val lifeSUpportRating = oxygenGeneratorRating * scrubberRating
+    return lifeSUpportRating
+}
+
+fun reduceIntoRating(inputs: List<Int>, bitCriteriaIsMostCommonBit: Boolean): Int {
+    var filteredInputs = inputs
+    var bitPosition = DIGIT_COUNT - 1
+    while (filteredInputs.size > 1) {
+        val mostCommonBit = mostCommonBitAtPosition(filteredInputs, bitPosition)
+        val wantedBitValue = if (bitCriteriaIsMostCommonBit) mostCommonBit else (1 - mostCommonBit)
+        filteredInputs = filteredInputs.filter(hasBitAtPosition(bit = wantedBitValue, position = bitPosition))
+        bitPosition--
+    }
+    return filteredInputs.single()
+}
+
+fun mostCommonBitAtPosition(inputs: List<Int>, position: Int): Int {
+    val mask = 1 shl position
+    // how many inputs have a 1 at the given position
+    val countOfOnesAtGivenPosition = inputs.filter { (it and mask) > 0 }.count()
+    val proportion: Double = countOfOnesAtGivenPosition.toDouble() / inputs.size
+    // using ">=" means a bias towards returning 1 if count of 1's and 0's is equal
+    if (proportion >= .5) return 1
+    return 0
+}
+
+
+fun hasBitAtPosition(bit: Int, position: Int): (Int) -> Boolean =
+    fun(x: Int): Boolean {
+        return (x shr position) and 1 == bit
+    }
