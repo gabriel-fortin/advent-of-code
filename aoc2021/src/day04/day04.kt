2911169f602a85286566876b8dca1eec6bf0747d
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2022-03-14 20:54:17 +0000

    Add 2021 - day 4 - part 2

--- aoc2021/src/day04/day04.kt
+++ aoc2021/src/day04/day04.kt
@@ -6,6 +6,9 @@ fun main() {
 
     val part1 = part1(drawnNumbers, bingoBoards)
     println("part1: $part1")
+
+    val part2 = part2(drawnNumbers, bingoBoards)
+    println("part2: $part2")
 }
 
 fun part1(drawnNumbers: List<Int>, bingoBoards: List<BingoBoard>): Int {
@@ -23,3 +26,16 @@ fun callNumber(number: Int, boards: List<BingoBoard>) {
     boards.forEach { it.tryMark(number) }
 }
 
+fun part2(drawnNumbers: List<Int>, allBingoBoards: List<BingoBoard>): Int {
+    val notFinishedBingoBoards = allBingoBoards.toMutableList()
+    for (drawnNumber in drawnNumbers) {
+        callNumber(drawnNumber, notFinishedBingoBoards)
+        if (notFinishedBingoBoards.size == 1) {
+            val lastWinningBoard = notFinishedBingoBoards.single()
+            if (lastWinningBoard.isBingo)
+                return drawnNumber * lastWinningBoard.sumUnmarkedNumbers()
+        }
+        notFinishedBingoBoards.removeIf { it.isBingo }
+    }
+    throw IllegalArgumentException("a board that wins last was not found")
+}
