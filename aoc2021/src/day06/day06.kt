3f942386a0ada6ace1a871daccd343204ba32b24
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2022-03-27 16:18:48 +0100

    Add 2021 - day 6 - part 1

--- aoc2021/src/day06/day06.kt
+++ aoc2021/src/day06/day06.kt
@@ -1,12 +1,30 @@
 package day06
 
 fun main() {
-    println("hello   ${getInput().substring(0..20)}")
-//    val input = getInput()
+    val input = getInput()
 
-//    val part1 = part1(input)
-//    println("part1: $part1")
+    val part1 = part1(input)
+    println("part1: $part1")
 
 //    val part2 = part2(input)
 //    println("part2: $part2")
+}
+
+const val SIMULATION_LENGTH = 80
+fun part1(input: List<Int>): Int {
+    val currentFish = input.toMutableList()
+    repeat(SIMULATION_LENGTH) {
+        currentFish.indices.forEach { i -> currentFish.updateFish(i) }
+    }
+    return currentFish.count()
+}
+
+private fun MutableList<Int>.updateFish(i: Int) {
+    when (this[i]) {
+        0 -> {
+            this[i] = 6
+            this.add(8)
+        }
+        else -> this[i]--
+    }
 }
--- aoc2021/src/day06/input.kt
+++ aoc2021/src/day06/input.kt
@@ -2,8 +2,16 @@ package day06
 
 import java.io.File
 
-fun getInput(): String {
+//fun getInput() = getTestInput()
+fun getInput() = getRealInput()
+
+@Suppress("unused")
+fun getTestInput() = listOf(3,4,3,1,2)
+
+fun getRealInput(): List<Int> {
     return File("input.txt")
         .readLines()
         .first()
+        .split(',')
+        .map(Integer::parseInt)
 }
