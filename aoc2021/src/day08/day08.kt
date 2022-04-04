6a4df013d91b7eb1967738d45a17492eaf8cce69
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2022-04-04 20:21:41 +0100

    Setup for 2021 - day 8

---
+++ aoc2021/src/day08/day08.kt
@@ -0,0 +1,5 @@
+package day08
+
+fun main() {
+    println("hello")
+}
---
+++ aoc2021/src/day08/input.kt
@@ -0,0 +1,15 @@
+package day08
+
+import java.io.File
+
+fun getInput(): Nothing = getTestInput()
+//fun getInput() = getRealInput()
+
+@Suppress("unused")
+fun getTestInput(): Nothing = TODO()
+
+//fun getRealInput(): List<Int> {
+//    return File("input.txt")
+//        .readLines()
+//
+//}
