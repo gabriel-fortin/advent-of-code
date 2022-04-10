fe79ef6b3a9114a1e7e347fea5eaf3c9422cb45a
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2022-04-10 19:53:38 +0100

    Add 2021 - day 8 - part 1

--- aoc2021/src/day08/day08.kt
+++ aoc2021/src/day08/day08.kt
@@ -1,6 +1,24 @@
 package day08
 
 fun main() {
-    println("hello")
-    println(getInput())
+    val input = getInput()
+
+    val part1 = part1(input)
+    println("part1: $part1")
+
+//    val part2 = part2(input)
+//    println("part2: $part2")
+}
+
+fun part1(entries: List<Entry>): Int {
+    return entries
+        .map(::count1478)
+        .sum()
+}
+
+/** Counts how many times digits 1, 4, 7, 8 occur in the value digits of the entry */
+fun count1478(entry: Entry): Int {
+    val unambiguousPatternLengths = listOf(2, 3, 4, 7)
+    return entry.valueDigits
+        .count { it.segments.size in unambiguousPatternLengths }
 }
