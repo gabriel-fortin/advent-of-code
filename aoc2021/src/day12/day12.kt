682209a39282938da7ad138c4d13c28466501b1f
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2022-05-22 19:47:35 +0100

    Make Graph and Node classes unspecific to the task at hand

--- aoc2021/src/day12/day12.kt
+++ aoc2021/src/day12/day12.kt
@@ -20,30 +20,39 @@ fun part1(pairs: List<Pair<String, String>>): Int {
 
 /**
  * assumption: input doesn't contain duplicated edges
+ * assumption: distinct nodes have distinct names
  */
-open class Graph(edgesList: List<Pair<String, String>>) {
-    val nodes = mutableMapOf<String, Node>()
-    init {
-        for ((name1, name2) in edgesList) {
-            // find node by name or create new
-            val node1 = nodes.getOrPut(name1) { Node(name1, name1 == name1.lowercase()) }
-            val node2 = nodes.getOrPut(name2) { Node(name2, name2 == name2.lowercase()) }
+open class Graph<TNode : Graph.Node<TNode>> {
+    val nodes = mutableMapOf<String, TNode>()
 
-            // we assume input doesn't have duplicated edges
-            node1.neighbours.add(node2)
-            node2.neighbours.add(node1)
-        }
+    fun putNode(key: String, node: TNode) = nodes.put(key, node)
+    fun getOrPutNode(key: String, defaultValue: () -> TNode) = nodes.getOrPut(key, defaultValue)
+
+    fun addEdge(node1: TNode, node2: TNode) {
+        node1.neighbours.add(node2)
+        node2.neighbours.add(node1)
     }
 
-    open class Node(val name: String, val isSmallCave: Boolean) {
-        val neighbours = mutableSetOf<Node>()
+    open class Node<TNode> {
+        val neighbours = mutableSetOf<TNode>()
     }
 }
 
-class PathsVisitingSmallCavesAtMostOnce(edgesList: List<Pair<String, String>>) : Graph(edgesList) {
-    private val validPaths = mutableListOf<Path>()
+class PathsVisitingSmallCavesAtMostOnce(edgesList: List<Pair<String, String>>) : Graph<Cave>() {
+    init {
+        for ((name1, name2) in edgesList) {
+            // find node by name or create new
+            val node1 = getOrPutNode(name1) { Cave(name1) }
+            val node2 = getOrPutNode(name2) { Cave(name2) }
 
-    fun findPaths(): List<Path> {
+            // we assume input doesn't have duplicated edges and don't check it
+            addEdge(node1, node2)
+        }
+    }
+
+    private val validPaths = mutableListOf<Path<Cave>>()
+
+    fun findPaths(): List<Path<Cave>> {
         if (validPaths.isNotEmpty()) throw IllegalStateException("This method is not meant to be run multiple times")
 
         val startNode = nodes["start"] ?: throw NoSuchElementException("Cannot find start node")
@@ -53,7 +62,7 @@ class PathsVisitingSmallCavesAtMostOnce(edgesList: List<Pair<String, String>>) :
     }
 
     // DFS on the graph
-    private fun findRecursively(pathSoFar: Path) {
+    private fun findRecursively(pathSoFar: Path<Cave>) {
         val lastNodeOnPath = pathSoFar.nodes.last()
         lastNodeOnPath.neighbours.forEach tryNode@{ newNode ->
             val extendedPath = pathSoFar + newNode
@@ -70,7 +79,11 @@ class PathsVisitingSmallCavesAtMostOnce(edgesList: List<Pair<String, String>>) :
     }
 }
 
-data class Path(val nodes: List<Graph.Node>) {
-    constructor(node: Graph.Node) : this(listOf(node))
-    operator fun plus(newNode: Graph.Node): Path = this.copy(nodes = nodes + newNode)
+class Cave(val name: String): Graph.Node<Cave>() {
+    val isSmallCave = name == name.lowercase()
+}
+
+data class Path<TNode>(val nodes: List<TNode>) {
+    constructor(node: TNode) : this(listOf(node))
+    operator fun plus(newNode: TNode): Path<TNode> = this.copy(nodes = nodes + newNode)
 }
