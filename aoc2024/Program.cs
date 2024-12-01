3048f3bfb6284589e39d83cab9419993c919f6c9
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2024-12-01 16:35:00 +0000

    Add 2024, day 01: input and part 1

--- aoc2024/Program.cs
+++ aoc2024/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Advent_of_Code_2024.day01;
 
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Day 01, part 1, example: " + new Day01().Part1(useExampleData: true));
+Console.WriteLine("Day 01, part 1, actual: " + new Day01().Part1(useExampleData: false));
+Console.WriteLine("Day 01, part 1, actual: " + new Day01().Part1UsingLotsOfLinqAndChaining());
---
+++ aoc2024/day01/Day01.cs
@@ -0,0 +1,68 @@
+﻿namespace Advent_of_Code_2024.day01;
+
+public partial class Day01
+{
+    public string Part1(bool useExampleData)
+    {
+        string rawInput = Input.GetInput(useExampleData);
+        var (list1, list2) = ParseInput(rawInput);
+
+        list1.Sort();
+        list2.Sort();
+
+        int distanceSum = 0;
+        for (int i = 0; i < list1.Count; i++)
+        {
+            int distance = Math.Abs(list1[i] - list2[i]);
+            distanceSum += distance;
+        }
+
+        return distanceSum.ToString();
+    }
+
+    private (List<int> list1, List<int> list2) ParseInput(string rawInput)
+    {
+        List<int> list1 = new();
+        List<int> list2 = new();
+
+        string[] rawLines = rawInput.Split('\n');
+        foreach (var rawLine in rawLines)
+        {
+            string[] rawNumbers = rawLine.Split("   ");
+            list1.Add(int.Parse(rawNumbers[0]));
+            list2.Add(int.Parse(rawNumbers[1]));
+        }
+
+        return (list1, list2);
+    }
+
+    public string Part1UsingLotsOfLinqAndChaining()
+    {
+        string rawInput = Input.GetInput(useExampleData: false);
+
+        int[][] numberPairs = rawInput
+            .Split('\n')
+            // split each line into an array of numbers
+            .Select(line => line
+                .Split("   ")  // IEnumerable<string>
+                .Select(int.Parse)  // IEnumerable<int>
+                .ToArray())  // int[]
+            .ToArray();
+
+        List<int> list1 = numberPairs.Select(x => x[0]).ToList();
+        List<int> list2 = numberPairs.Select(x => x[1]).ToList();
+
+        list1.Sort();
+        list2.Sort();
+
+        // get corresponding numbers together
+        return list1.Zip(list2, (x, y) => (int[]) [x, y])  // IEnumerable<int[]>
+            // calculate difference
+            .Select(x => x[0] - x[1])  // IEnumerable<int>
+            // calculate absolute value of the difference
+            .Select(Math.Abs)  // IEnumerable<int>
+            // sum all the differences
+            .Aggregate((x, y) => x + y)  // int
+            .ToString();
+    }
+}
