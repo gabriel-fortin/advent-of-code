17f7f1acd80f4278a00be72e7b36587ff3372469
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2024-12-02 21:01:51 +0000

    Add 2024, day 02: input and part 1

--- aoc2024/Program.cs
+++ aoc2024/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Advent_of_Code_2024.day01;
+using Advent_of_Code_2024.day02;
 
 
 Console.WriteLine("Day 01, part 1, example: " + new Day01().Part1(useExampleData: true));
@@ -7,3 +8,8 @@ Console.WriteLine("Day 01, part 1, actual: " + new Day01().Part1UsingLotsOfLinqA
 
 Console.WriteLine("Day 01, part 2, example: " + new Day01().Part2(useExampleData: true));
 Console.WriteLine("Day 01, part 2, actual: " + new Day01().Part2(useExampleData: false));
+Console.WriteLine("");
+
+
+Console.WriteLine("Day 02, part 1, example: " + new Day02().Part1(useExampleData: true));
+Console.WriteLine("Day 02, part 1, actual: " + new Day02().Part1(useExampleData: false));
---
+++ aoc2024/day02/Day02.cs
@@ -0,0 +1,52 @@
+﻿namespace Advent_of_Code_2024.day02;
+
+public partial class Day02
+{
+    public string Part1(bool useExampleData)
+    {
+        string rawInput = Input.GetInput(useExampleData);
+        
+        Report[] reports = rawInput
+            .Split("\n")
+            .Select(ParseReport)
+            .ToArray();
+
+        return reports
+            .Count(x => x.IsSafe())
+            .ToString();
+    }
+
+    private Report ParseReport(string line)
+    {
+        List<Level> levels = line
+            .Split(" ")
+            .Select(int.Parse)
+            .Select(x => new Level(x))
+            .ToList();
+        return new Report(levels);
+    }
+}
+
+public class Report(List<Level> levels)
+{
+    public bool IsSafe()
+    {
+        // from a list of [1,2,3,4,...] make a list of [(1,2), (2,3), (3,4), ...]
+        (Level, Level)[] adjacentLevelPairs = levels.SkipLast(1).Zip(levels.Skip(1)).ToArray();
+
+        bool areAllIncreasing = adjacentLevelPairs
+            .Aggregate(true, (current, nextPair) => current && nextPair.Item1 < nextPair.Item2);
+        bool areAllDecreasing = adjacentLevelPairs
+            .Aggregate(true, (current, nextPair) => current && nextPair.Item1 > nextPair.Item2);
+        bool haveAllGoodDistance = adjacentLevelPairs
+            .Select(pair => Math.Abs(pair.Item1 - pair.Item2))
+            .Aggregate(true, (current, nextDiff) => current && nextDiff is >= 1 and <= 3);
+
+        return (areAllIncreasing || areAllDecreasing) && haveAllGoodDistance;
+    }
+}
+
+public record Level(int Value)
+{
+    public static implicit operator int(Level level) => level.Value;
+}
