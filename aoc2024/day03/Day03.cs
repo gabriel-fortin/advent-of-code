f8a0a60c65ca5b94fd1af3866569b05badd6807b
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2024-12-04 21:25:03 +0000

    Add 2024, day 03: input and part 1

---
+++ aoc2024/day03/Day03.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Advent_of_Code_2024.day03;
+
+public static partial class Day03
+{
+    private static readonly Regex MulStructure = new(@"mul\((\d{1,3}),(\d{1,3})\)");
+    
+    public static string Part1(bool useExampleData)
+    {
+        string rawInput = Input.GetInput(useExampleData);
+
+        return MulStructure
+            .Matches(rawInput)
+            .Select(match => new Mul(match.Value).ComputeValue())
+            .Aggregate(0L, (acc, cur) => acc + cur)
+            .ToString();
+    }
+
+    // public static string Part2(bool useExampleData)
+    // {
+    // }
+
+    public record Mul
+    {
+        public int X { get; }
+        
+        public int Y { get; }
+
+        public Mul(string mulText)
+        {
+            Match match = MulStructure.Match(mulText);
+            if (!match.Success) throw new ArgumentException($"Invalid mul text: {mulText}");
+            
+            X = int.Parse(match.Groups[1].Value);
+            Y = int.Parse(match.Groups[2].Value);
+        }
+
+        public int ComputeValue() => X * Y;
+    }
+}
