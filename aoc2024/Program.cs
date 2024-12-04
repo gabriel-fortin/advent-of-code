2b5f3b5333e93eaef0b4cc13fdd2a0b83822e066
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2024-12-04 22:27:03 +0000

    Add 2024, day 03: part 2

--- aoc2024/Program.cs
+++ aoc2024/Program.cs
@@ -2,24 +2,21 @@
 using Advent_of_Code_2024.day02;
 using Advent_of_Code_2024.day03;
 
-
+Console.WriteLine("== Day 01 ==");
 Console.WriteLine("Day 01, part 1, example: " + new Day01().Part1(useExampleData: true));
 Console.WriteLine("Day 01, part 1, actual: " + new Day01().Part1(useExampleData: false));
 Console.WriteLine("Day 01, part 1, actual: " + new Day01().Part1UsingLotsOfLinqAndChaining());
 Console.WriteLine("Day 01, part 2, example: " + new Day01().Part2(useExampleData: true));
 Console.WriteLine("Day 01, part 2, actual: " + new Day01().Part2(useExampleData: false));
-Console.WriteLine("");
 
-
+Console.WriteLine("== Day 02 ==");
 Console.WriteLine("Day 02, part 1, example: " + new Day02().Part1(useExampleData: true));
 Console.WriteLine("Day 02, part 1, actual: " + new Day02().Part1(useExampleData: false));
 Console.WriteLine("Day 02, part 2, example: " + new Day02().Part2(useExampleData: true));
 Console.WriteLine("Day 02, part 2, actual: " + new Day02().Part2(useExampleData: false));
-Console.WriteLine("");
 
-
+Console.WriteLine("== Day 03 ==");
 Console.WriteLine("Day 03, part 1, example: " + Day03.Part1(useExampleData: true));
 Console.WriteLine("Day 03, part 1, actual: " + Day03.Part1(useExampleData: false));
-// Console.WriteLine("Day 03, part 2, example: " + Day03.Part2(useExampleData: true));
-// Console.WriteLine("Day 03, part 2, actual: " + Day03.Part2(useExampleData: false));
-Console.WriteLine("");
+Console.WriteLine("Day 03, part 2, example: " + Day03.Part2(useExampleData: true));
+Console.WriteLine("Day 03, part 2, actual: " + Day03.Part2(useExampleData: false));
--- aoc2024/day03/Day03.cs
+++ aoc2024/day03/Day03.cs
@@ -4,38 +4,82 @@ namespace Advent_of_Code_2024.day03;
 
 public static partial class Day03
 {
+    private const string DO = "do()";
+    private const string DONT = "don't()";
     private static readonly Regex MulStructure = new(@"mul\((\d{1,3}),(\d{1,3})\)");
-    
+
     public static string Part1(bool useExampleData)
     {
-        string rawInput = Input.GetInput(useExampleData);
+        string rawInput = Input.GetInputForPart1(useExampleData);
 
         return MulStructure
             .Matches(rawInput)
-            .Select(match => new Mul(match.Value).ComputeValue())
-            .Aggregate(0L, (acc, cur) => acc + cur)
+            .Select(match => new Mul(match))
+            .Aggregate(0L, (acc, mul) => acc + mul.ComputedValue)
             .ToString();
     }
 
-    // public static string Part2(bool useExampleData)
-    // {
-    // }
+    public static string Part2(bool useExampleData)
+    {
+        string rawInput = Input.GetInputForPart2(useExampleData);
+
+        bool isMulEnabled = true;
+        int inputIndex = 0;
+        List<Mul> enabledMuls = new();
+
+        while (inputIndex < rawInput.Length)
+        {
+            Match mul = MulStructure.Match(rawInput, inputIndex);
+            int doIndex = rawInput.IndexOf(DO, inputIndex);
+            int dontIndex = rawInput.IndexOf(DONT, inputIndex);
+            // "IndexOf" returns -1 if it doesn't find the requested string
+            // but handling -1 would make conditions longer and less clear
+            // so we replace -1's with an index somewhere after the end of the input
+            if (doIndex == -1) doIndex = rawInput.Length;
+            if (dontIndex == -1) dontIndex = rawInput.Length;
+
+            // if there are no more "mul" instructions then there's nothing more to add to the list
+            if (!mul.Success) break;
+
+            // if mul instructions are disabled, we just find where they are enabled again
+            // if they are enabled, we need to find out what comes first: a "don't" or a "mul"
+            if (!isMulEnabled)
+            {
+                inputIndex = doIndex + DO.Length;
+                isMulEnabled = true;
+            }
+            else if (dontIndex < mul.Index)
+            {
+                inputIndex = dontIndex + DONT.Length;
+                isMulEnabled = false;
+            }
+            else
+            {
+                inputIndex = mul.Index + mul.Length;
+                enabledMuls.Add(new Mul(mul));
+            }
+        }
+
+        return enabledMuls
+            .Sum(x => x.ComputedValue)
+            .ToString();
+    }
 
     public record Mul
     {
-        public int X { get; }
-        
-        public int Y { get; }
+        public int ComputedValue { get; }
 
-        public Mul(string mulText)
+        public Mul(string mulText) : this(MulStructure.Match(mulText))
         {
-            Match match = MulStructure.Match(mulText);
-            if (!match.Success) throw new ArgumentException($"Invalid mul text: {mulText}");
-            
-            X = int.Parse(match.Groups[1].Value);
-            Y = int.Parse(match.Groups[2].Value);
         }
 
-        public int ComputeValue() => X * Y;
+        public Mul(Match match)
+        {
+            if (!match.Success) throw new ArgumentException($"Invalid mul text: {match.Value}");
+
+            int x = int.Parse(match.Groups[1].Value);
+            int y = int.Parse(match.Groups[2].Value);
+            ComputedValue = x * y;
+        }
     }
 }
