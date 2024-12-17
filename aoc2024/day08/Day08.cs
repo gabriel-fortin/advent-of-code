947f6dfc8a08b7f577cf1b0cbcce5caf690bf190
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2024-12-17 21:54:36 +0000

    Add 2024, day 08: part 2

--- aoc2024/day08/Day08.cs
+++ aoc2024/day08/Day08.cs
@@ -28,7 +28,34 @@ public static partial class Day08
 
     public static string Part2(bool useExampleData)
     {
-        return "NOT IMPLEMENTED";
+        string rawInput = Input.GetInput(useExampleData);
+        var city = new Matrix(rawInput.Split("\r\n"));
+
+        return city.AllPositions()
+            .Where(x => char.IsDigit(x.letter) || char.IsLetter(x.letter))
+            .GroupBy(x => x.letter)
+            .Select(x => x.Select(y => y.position).ToList())
+            .SelectMany(ComputeAllPermutations)
+            // generate all nodes lying on a line created by each pair of antennas
+            .SelectMany(antennaePair =>
+            {
+                (Pos pos1, Pos pos2) = antennaePair;
+                
+                // infinite sequence of antinodes in one direction
+                IEnumerable<Pos> antiNodes1 = ComputeAntiNodesDirectionally(pos1, pos2)
+                    // but take them for only as long as they are within city's borders
+                    .TakeWhile(pos => city.Get(pos) != null);
+                
+                // infinite sequence of antennas in the other direction
+                IEnumerable<Pos> antiNodes2 = ComputeAntiNodesDirectionally(pos2, pos1)
+                    // again, stop taking them when they get outside the city
+                    .TakeWhile(pos => city.Get(pos) != null);
+                
+                return antiNodes1.Concat(antiNodes2);
+            })
+            .Distinct()
+            .Count(antiNode => city.Get(antiNode) != null)
+            .ToString();
     }
 
     private static IEnumerable<(Pos, Pos)> ComputeAllPermutations(List<Pos> positions)
@@ -48,5 +75,19 @@ public static partial class Day08
         var (pos1, pos2) = positionPair;
         yield return pos2.MoveBy(new Move(pos2.X - pos1.X, pos2.Y - pos1.Y));
         yield return pos1.MoveBy(new Move(pos1.X - pos2.X, pos1.Y - pos2.Y));
+    }
+
+    /// Computes an infinite sequence of antinodes in a certain direction
+    private static IEnumerable<Pos> ComputeAntiNodesDirectionally(Pos pos1, Pos pos2)
+    {
+        var vector = new Move(pos2.X - pos1.X, pos2.Y - pos1.Y);
+
+        Pos pos = pos2;
+        while (true)
+        {
+            yield return pos;
+            pos = pos.MoveBy(vector);
+        }
+        // ReSharper disable once IteratorNeverReturns
     }
 }
