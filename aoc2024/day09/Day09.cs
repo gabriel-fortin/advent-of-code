58bea48735f65a30524e99eff98580ade9befb6e
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2024-12-18 22:43:03 +0000

    Add 2024, day 09: part 2

--- aoc2024/day09/Day09.cs
+++ aoc2024/day09/Day09.cs
@@ -8,13 +8,18 @@ public static partial class Day09
         var disk = new Disk(rawInput);
 
         disk.DecompressFromDenseFormat();
-        disk.CompactFiles();
+        disk.CompactFilesByMovingSingleBlocks();
         return disk.ComputeChecksum().ToString();
     }
 
     public static string Part2(bool useExampleData)
     {
-        return "NOT IMPLEMENTED";
+        string rawInput = Input.GetInput(useExampleData);
+        var disk = new Disk(rawInput);
+
+        disk.DecompressFromDenseFormat();
+        disk.CompactFilesByMovingWholeFiles();
+        return disk.ComputeChecksum().ToString();
     }
 }
 
@@ -61,7 +66,7 @@ public class Disk
         }
     }
 
-    public void CompactFiles()
+    public void CompactFilesByMovingSingleBlocks()
     {
         int emptySpaceIndex = 0;
         int lastDataIndex = _dataSize - 1;
@@ -71,7 +76,7 @@ public class Disk
             FindNextEmptySpace();
             FindLastOccupiedBlock();
             if (emptySpaceIndex > lastDataIndex) break;
-            
+
             _data[emptySpaceIndex] = _data[lastDataIndex];
             _data[lastDataIndex] = null;
             // Visualise();
@@ -87,6 +92,68 @@ public class Disk
         void FindLastOccupiedBlock()
         {
             while (_data[lastDataIndex] == null) lastDataIndex--;
+        }
+    }
+
+    public void CompactFilesByMovingWholeFiles()
+    {
+        // we know that last block is always occupied by a file block
+        int fileId = _data[_dataSize - 1]!.Id;
+
+        for (; fileId >= 0; fileId--)
+        {
+            // Visualise();
+            Range file = FindFile(fileId);
+            int emptySpaceLocation = FindLeftmostFreeSpaceFor(file);
+            if (emptySpaceLocation > file.Start.Value) continue;
+
+            if (emptySpaceLocation != -1)
+            {
+                MoveFile(file, emptySpaceLocation);
+            }
+        }
+
+        return;
+
+        Range FindFile(int id)
+        {
+            // end indicates the position AFTER the last block of the file
+            int end = _dataSize;
+            while (_data[end - 1]?.Id != id) end--;
+
+            int start = end;
+            while (start > 0 && _data[start - 1]?.Id == id) start--;
+
+            return new Range(start, end);
+        }
+
+        int FindLeftmostFreeSpaceFor(Range file)
+        {
+            int requiredSpace = file.End.Value - file.Start.Value;
+
+            int searchIndex = 0;
+            while (searchIndex < file.Start.Value)
+            {
+                int spaceStart = Array.FindIndex(_data, searchIndex, block => block == null);
+                if (spaceStart == -1) return -1;
+
+                int spaceEnd = Array.FindIndex(_data, spaceStart, block => block != null);
+                if (spaceEnd == -1) return -1;
+
+                if (spaceEnd - spaceStart >= requiredSpace) return spaceStart;
+                searchIndex = spaceEnd;
+            }
+
+            return -1;
+        }
+
+        void MoveFile(Range file, int emptySpaceLocation)
+        {
+            for (int j = emptySpaceLocation, i = file.Start.Value; i < file.End.Value; i++, j++)
+            {
+                _data[j] = _data[i];
+                _data[i] = null;
+            }
         }
     }
 
