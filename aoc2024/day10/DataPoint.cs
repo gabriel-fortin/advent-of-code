54e24416971471e8ce1f53a3c18b1d355719170b
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2024-12-23 17:23:45 +0000

    Add 2024, day 10: refactor

---
+++ aoc2024/day10/DataPoint.cs
@@ -0,0 +1,18 @@
+﻿using Advent_of_Code_2024.day04;
+
+namespace Advent_of_Code_2024.day10;
+
+public class DataPoint(int height = 0)
+{
+    public int Height { get; } = height;
+
+    // using a set which has elements of a record type allows to avoid duplicates
+    public HashSet<Pos> Destinations { get; } = new();
+    
+    public int GetScore()
+    {
+        return Destinations.Count;
+    }
+
+    public static DataPoint Parse(char input) => new(height: input - '0');
+}
--- aoc2024/day10/Day10.cs
+++ aoc2024/day10/Day10.cs
@@ -16,53 +16,12 @@ public static partial class Day10
     public static string Part1(bool useExampleData)
     {
         string rawInput = Input.GetInput(useExampleData);
-        Matrix<Data> matrix = ParseTopographicMap(rawInput);
+        Matrix<DataPoint> matrix = ParseTopographicMap(rawInput);
 
-        // We'll use dynamic programming.
-        // To compute data for a position of a certain height
-        // we'll use data from positions of a greater height (previous computations)
-
-        // order all positions by height, from '0' to '9'
-        IGrouping<int, (Pos position, Data element)>[] byHeight = matrix
-            .AllPositions()
-            .GroupBy(x => x.element.Height)
-            .OrderBy(x => x.Key)
-            .ToArray();
-
-        // set destination for highest positions - each is set to itself
-        foreach ((Pos ownPosition, Data element) in byHeight[9])
-        {
-            element.Destinations.Add(ownPosition);
-        }
-
-        // Visualise(matrix, 9);
-
-        // for each lower position, use destinations of neighbouring higher positions as its destinations
-        for (int i = 8; i >= 0; i--)
-        {
-            foreach (var (position, element) in byHeight[i])
-            {
-                IEnumerable<Pos> destinationsOfNextHeightNeighbours = NeighbourMoves
-                    // compute neighbouring positions
-                    .Select(move => position.MoveBy(move))
-                    // for each position, get the data for that position
-                    .Select(pos => matrix.Get(pos))
-                    // only consider neighbours withing bounds and of the next height
-                    .Where(data => data != null && data.Height == i + 1)
-                    // get all destinations from those neighbours
-                    // (we know data is not null because we excluded nulls in the line above)
-                    .SelectMany(data => data!.Destinations);
-                foreach (Pos destination in destinationsOfNextHeightNeighbours)
-                {
-                    element.Destinations.Add(destination);
-                }
-            }
-
-            // Visualise(matrix, i);
-        }
+        var dataPointsByHeight = ProcessData(matrix);
 
         // return sum of scores of '0' positions
-        return byHeight[0]
+        return dataPointsByHeight[0]
             .Sum(x => x.element.GetScore())
             .ToString();
     }
@@ -72,24 +31,70 @@ public static partial class Day10
         return "NOT IMPLEMENTED";
     }
 
-    private static Matrix<Data> ParseTopographicMap(string rawInput)
+    private static IGrouping<int, (Pos position, DataPoint element)>[] ProcessData(Matrix<DataPoint> matrix)
     {
-        Data[][] data = rawInput
+        // We'll use dynamic programming.
+        // To compute data for a position of a certain height
+        // we'll use data from neighbours having the next height (previous computations)
+
+        // order all positions/data points by height, from '0' to '9'
+        IGrouping<int, (Pos position, DataPoint element)>[] byHeight = matrix
+            .AllPositions()
+            .GroupBy(x => x.element.Height)
+            .OrderBy(x => x.Key)
+            .ToArray();
+
+        // recursion base: set destination for highest positions - each is set to itself
+        foreach ((Pos ownPosition, DataPoint element) in byHeight[9])
+        {
+            element.Destinations.Add(ownPosition);
+        }
+        // Visualise(matrix, 9);
+
+        // for each lower position's destination use destinations of neighbouring next level positions
+        for (int currentHeight = 8; currentHeight >= 0; currentHeight--)
+        {
+            foreach (var (position, element) in byHeight[currentHeight])
+            {
+                IEnumerable<Pos> destinationsOfNextHeightNeighbours = NeighbourMoves
+                    // compute neighbouring positions
+                    .Select(move => position.MoveBy(move))
+                    // for each position, get the data for that position
+                    .Select(pos => matrix.Get(pos))
+                    // only consider neighbours withing bounds and of the next height
+                    .Where(data => data != null && data.Height == currentHeight + 1)
+                    // get all destinations from those neighbours
+                    // (we know data is not null because we excluded nulls in the line above)
+                    .SelectMany(data => data!.Destinations);
+                foreach (Pos destination in destinationsOfNextHeightNeighbours)
+                {
+                    element.Destinations.Add(destination);
+                }
+            }
+            // Visualise(matrix, i);
+        }
+
+        return byHeight;
+    }
+
+    private static Matrix<DataPoint> ParseTopographicMap(string rawInput)
+    {
+        DataPoint[][] data = rawInput
             .Split("\r\n")
             .Select(ParseLine)
             .ToArray();
 
-        return new Matrix<Data>(data);
+        return new Matrix<DataPoint>(data);
 
-        Data[] ParseLine(string line) =>
+        DataPoint[] ParseLine(string line) =>
             line
-                .Select(Data.Parse)
+                .Select(DataPoint.Parse)
                 .ToArray();
     }
 
-    private static void Visualise(Matrix<Data> matrix, int height)
+    private static void Visualise(Matrix<DataPoint> matrix, int height)
     {
-        foreach (IGrouping<int, (Pos position, Data element)> line in matrix
+        foreach (IGrouping<int, (Pos position, DataPoint element)> line in matrix
                      .AllPositions()
                      .GroupBy(x => x.position.X))
         {
@@ -118,19 +123,4 @@ public static partial class Day10
 
         Console.WriteLine();
     }
-}
-
-public class Data(int height = 0)
-{
-    public int Height { get; } = height;
-
-    // using a set which has elements of a record type allows to avoid duplicates
-    public HashSet<Pos> Destinations { get; } = new();
-    
-    public int GetScore()
-    {
-        return Destinations.Count;
-    }
-
-    public static Data Parse(char input) => new(height: input - '0');
 }
